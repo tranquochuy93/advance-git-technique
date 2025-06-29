225a6db49325ea1841d14706a9689e5e8451d452
Huy Tran Quoc <huy.tq@hodfords.com>
2025-06-29 10:19:01 +0700

    feat(RAG-6): update review introduce
    feat(RAG-4): create shared branch
    
    feat(RAG-4): create review from reading csv file
    
    feat(RAG-4): add csv file

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ from vector import retriever
 model = OllamaLLM(model="llama3.2")
 
 template = """
-You are an expert in answering questions about a pizza restaurant
+This is the answering questions about a pizza restaurant
 
 Here are some relevant reviews: {reviews}
 
 Here is the question to answer: {question}
 """
+
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
-question = input("Ask your question (q to quit): ")
-result = chain.invoke({"reviews": [], "question": question})
-print(result)
+while True:
+    print("\n\n-------------------------------")
+    question = input("Input your question (q to quit): ")
+    print("\n\n")
+    if question == "q":
+        break
+    
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
+    print(result)
